c99ecb4eb182e1209bffdef7d3be904a7576aee2
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 10:29:16 +0000

    Mon, Mar 11, 2024, 3:59 PM +05:30

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
+typedef struct
+{
+    int x, y;
+    short life;
+    char *name;
+} Man;
+
 int main(int argc, const char *argv[])
 {
-    int const array[10] = {10,2,3,4,5,6,78,9,4,6};
-
-    for(int i=0; i<10; i++)
-    {
-        printf("%d ", array[i]);
-    }
 
     return 0;
 }
