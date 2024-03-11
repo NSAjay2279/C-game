3e212f39ad79af9b6a29d35b9cbddd7e24928688
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 06:56:19 +0000

    Mon, Mar 11, 2024, 12:26 PM +05:30

--- main.c
+++ main.c
@@ -10,17 +10,33 @@ void explainVariables()
     long whatever = 0; // 4 byte
     long long whatever2 = 0; //8bye 64-bit
 
+    //unsigned
+    unsigned int i1= 0;  //0 to 4 billion
+    unsigned char c1 = 0; //0..255
+
     //floating point (decimals)
     float w = 1.5f;     //4 byte
     double q = 1.50000000000000000000001;     //8 byte
-    float w2;
 
+    float w2;
     w = 3.0f;
+
+    char *str = "phrases of characters... banana";
+}
+
+void doSomething2()
+{
+    int x = 2, y = 5;
+
+    x += y;
+
+    printf("Result is: %d\n", x);
 }
 
 int main(int argc, const char *argv[])
 {
     explainVariables();
+    doSomething2();
 
     printf("Words\n");
 
