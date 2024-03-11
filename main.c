5c456ea4e6e7682288f2de06b4bf9000bc76909a
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 08:26:44 +0000

    Mon, Mar 11, 2024, 1:56 PM +05:30

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-short halveIt(short number[])
-{
-    number[0] = 3;
-    return number[1];
-}
-
 int main(int argc, const char *argv[])
 {
-    printf("*** Welcome to my damn program! ***\n");
+    int const array[10] = {10,2,3,4,5,6,78,9,4,6};
 
-    short array[1] = {2};
-    array[0] = halveIt(array);
-
-    printf("the value of array[0] is: %hd", array[0]);
+    for(int i=0; i<10; i++)
+    {
+        printf("%d ", array[i]);
+    }
 
     return 0;
 }
