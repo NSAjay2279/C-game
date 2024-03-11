1c2f58d937c11d01ffeb878fcd497e37f1c02873
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 10:33:05 +0000

    Mon, Mar 11, 2024, 4:03 PM +05:30

--- main.c
+++ main.c
@@ -9,6 +9,17 @@ typedef struct
 
 int main(int argc, const char *argv[])
 {
+    Man man, man2;
+    man.x = 50;
+    man.y = 50;
+    man.life = 100;
+    man.name = "a man";
 
+    man2.x = 50;
+    man2.y = 100;
+    man2.life = 100;
+    man2.name = "a man 2";
+
+    printf("Name of man2: %s\n", man2.name);
     return 0;
 }
