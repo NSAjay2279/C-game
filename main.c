a2e14afc3388e48b233aac4cf775cb5cfd6e12d5
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 07:25:43 +0000

    Mon, Mar 11, 2024, 12:55 PM +05:30

--- main.c
+++ main.c
@@ -1,50 +1,20 @@
-// main.c
+#include <stdio.h>
 
-#include <stdio.h> //stdio api
-
-
-//Operators
-void doSomethingWithOperators()
+int doubleIt(int number)
 {
-    int x = 5, y = 2;
+    rerurn number*2;
+}
 
-    x = 5;
-    x = x + y;
-    y = x - y;
-    x = x * 5;
-    y = x / 5;
-
-    x += y;
-    x *= y;
-    x/= 5;
-    x-= y;
-
-    x = 5;
-    int z = x++;
-    x = 5;
-    z = ++x;
-
-    x = x - 1;
-    x--;
-
-    x = 1;
-    x = !x;
-    printf("x is %d\n", x);
-
-    //? :
-
-
-    printf("Result is: %d\n", x);
+int halveIt(int number)
+{
+    return number/2;
 }
 
 int main(int argc, const char *argv[])
 {
     printf("*** Welcome to my damn program!  ***\n");
 
-    doSomethingWithOperators();
 
-    //Wait for user to hit enter before quitting
-    //getchar();
 
     return 0;
 }
