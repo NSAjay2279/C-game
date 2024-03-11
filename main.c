d95fc4ba8f913ec65ace8637ec151676ac135715
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2024-03-11 10:54:17 +0000

    Mon, Mar 11, 2024, 4:24 PM +05:30

--- main.c
+++ main.c
@@ -7,16 +7,6 @@ typedef struct
     char *name;
 } Man;
 
-void structs()
-{
-    Man man[100];
 
-    for(int i-)
-}
 int main(int argc, const char *argv[])
-{
-
-
-    printf("Name of man2: %s\n", man2.name);
-    return 0;
-}
+{}
